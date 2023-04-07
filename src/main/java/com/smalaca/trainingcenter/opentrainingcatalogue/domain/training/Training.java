ae95c0627304edcad570574d1f56f1232e504ecc
smalaca <ab13.krakow@gmail.com>
2023-04-07 17:07:04 +0200

    ISSUE-1 suppress unwritten and unread fields

--- src/main/java/com/smalaca/trainingcenter/opentrainingcatalogue/domain/training/Training.java
+++ src/main/java/com/smalaca/trainingcenter/opentrainingcatalogue/domain/training/Training.java
@@ -4,11 +4,13 @@ import com.smalaca.libraries.annotation.architecture.portandadapters.PrimaryPort
 import com.smalaca.libraries.annotation.domaindrivendesign.AggregateRoot;
 import com.smalaca.libraries.annotation.domaindrivendesign.DomainFactory;
 import com.smalaca.trainingcenter.opentrainingcatalogue.domain.offer.Offer;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.util.UUID;
 
 @AggregateRoot
 public class Training {
+    @SuppressFBWarnings("UWF_UNWRITTEN_FIELD")
     private UUID trainingId;
     private TrainingProgrammeCode trainingProgrammeCode;
 
