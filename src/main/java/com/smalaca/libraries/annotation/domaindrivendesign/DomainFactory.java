c62c0a18d26471e41759011f2a75895230322de7
smalaca <ab13.krakow@gmail.com>
2023-04-07 16:56:17 +0200

    ISSUE-1 rename Factory annotation into DomainFactory and make it available on method level

--- src/main/java/com/smalaca/libraries/annotation/domaindrivendesign/DomainFactory.java
+++ src/main/java/com/smalaca/libraries/annotation/domaindrivendesign/DomainFactory.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface Factory {
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface DomainFactory {
 }
