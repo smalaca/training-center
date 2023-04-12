af427a1f768a5208c68b7d086a978c8a04f31f6c
smalaca <ab13.krakow@gmail.com>
2023-04-12 21:47:14 +0200

    ISSUE-18 fix Price equals method

--- src/main/java/com/smalaca/trainingcenter/opentrainingcatalogue/domain/price/Price.java
+++ src/main/java/com/smalaca/trainingcenter/opentrainingcatalogue/domain/price/Price.java
@@ -5,6 +5,7 @@ import com.smalaca.libraries.annotation.domaindrivendesign.ValueObject;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @ValueObject
 public final class Price {
@@ -21,22 +22,40 @@ public final class Price {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
 
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Price price = (Price) o;
 
-        return
-                (value == null && price.value == null) ||
-                (value != null && price.value != null && value.compareTo(price.value) == 0);
+        if (hasNoPrice() && price.hasNoPrice()) {
+            return true;
+        }
+
+        return hasSamePriceAs(price);
+    }
+
+    private boolean hasNoPrice() {
+        return value == null;
+    }
+
+    private boolean hasSamePriceAs(Price price) {
+        return hasPrice() && price.hasPrice() && value.compareTo(price.value) == 0;
+    }
+
+    private boolean hasPrice() {
+        return value != null;
     }
 
     @Override
+    @SuppressWarnings("MagicNumber")
     public int hashCode() {
-        BigDecimal object = value == null ? null : value.stripTrailingZeros();
         return new HashCodeBuilder(17, 37)
-                .append(object)
+                .append(Optional.ofNullable(value).map(BigDecimal::stripTrailingZeros))
                 .toHashCode();
     }
 }
