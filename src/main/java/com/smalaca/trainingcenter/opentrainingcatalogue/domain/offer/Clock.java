77ab0a6bcdc364b5de40510ff53430021b73aeac
smalaca <ab13.krakow@gmail.com>
2023-04-16 14:04:51 +0200

    ISSUE-31 Choose Training - annotate Clock as secondary port

--- src/main/java/com/smalaca/trainingcenter/opentrainingcatalogue/domain/offer/Clock.java
+++ src/main/java/com/smalaca/trainingcenter/opentrainingcatalogue/domain/offer/Clock.java
@@ -1,7 +1,10 @@
 package com.smalaca.trainingcenter.opentrainingcatalogue.domain.offer;
 
+import com.smalaca.libraries.annotation.architecture.portandadapters.SecondaryPort;
+
 import java.time.LocalDate;
 
+@SecondaryPort
 public interface Clock {
     LocalDate nowDate();
 }
